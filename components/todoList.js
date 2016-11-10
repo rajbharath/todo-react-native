4a0ab241a3fd419a3e8bacd04c6e3bad4cd063ac
rajbharath <rbrajbharath1@gmail.com>
2016-11-11 00:58:16 +0530

    Displaying Todo

--- components/todoList.js
+++ components/todoList.js
@@ -1,11 +1,49 @@
-import React, { Component } from 'react';
-import { ListView, Text } from 'react-native';
+import React, {Component} from 'react';
+import  {
+  ListView,
+  Text,
+  StyleSheet
+} from 'react-native';
 
 export default class TodoList extends Component {
   constructor () {
-    super()
+    super();
+    this.state = {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (r1, r2) => r1 !== r2
+      })
+    };
+
+    this.state.dataSource = this.state.dataSource.cloneWithRows(
+        [
+          { id: 1, text: 'Learn React', completed: false },
+          { id: 2, text: 'Learn React Native', completed: false }
+        ]
+    );
+  }
+
+  renderRow(todo) {
+    return (
+            <Text
+              style = {styles.row}
+            >
+              {todo.text}
+            </Text>
+            );
   }
   render () {
-    return (<Text>Helloworld</Text>)
+    return (<ListView
+            dataSource={this.state.dataSource}
+            renderRow={this.renderRow}>
+            </ListView>
+            )
   }
 }
+
+const styles = StyleSheet.create({
+  row: {
+    flex: 1,
+    alignItems: 'center',
+    padding: 10
+  }
+});
