f309624fb48fef9b4a45cb27f3bb19c6d090e377
rajbharath <rbrajbharath1@gmail.com>
2016-11-11 00:45:12 +0530

    Initial Draft

---
+++ components/todoList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { ListView, Text } from 'react-native';
+
+export default class TodoList extends Component {
+  constructor () {
+    super()
+  }
+  render () {
+    return (<Text>Helloworld</Text>)
+  }
+}
---
+++ index.ios.js
@@ -0,0 +1,8 @@
+import TodoList from './components/todoList';
+
+import {
+  AppRegistry
+} from 'react-native';
+
+
+AppRegistry.registerComponent('TodoList', () => TodoList);
